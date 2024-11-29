43e4df42a1db9adb64cd0eb5fbf917dbdffa3c29
kunal Lingwal <kunallingwal2@gmail.com>
2024-11-29 18:15:24 +0530

    Update withtrack_whatsapp.js

--- Controller/withtrack_whatsapp.js
+++ Controller/withtrack_whatsapp.js
@@ -4,7 +4,6 @@ const auth = require("../Middleware/mongoAuth");
 const urlShortLinkModel = require("../Models/urlShortLinkSchema"); 
 
 router.all("/api_click_report", auth, async (req, res) => {
-  try {
 
    let resdata;
     if (req.method === "GET") {
@@ -14,78 +13,71 @@ router.all("/api_click_report", auth, async (req, res) => {
     if (req.method === "POST") {
       resdata = req.body;
     }
+   
+    if(resdata.method === "insert_api_wpbtnclick"){
 
-    if (resdata.method !== "insert_api_wpbtnclick") {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid method",
-      });
+        const {
+          phone_number,
+          user_id,
+          sender,
+          main_url,
+          short_url,
+          urlkey,
+          created,
+          ip,
+          url_clickcount,
+          url_city,
+          url_device,
+          camp_id,
+          submit_via,
+          country_code,
+        } = resdata;
+    
+        if (
+          !phone_number ||
+          !user_id ||
+          !sender ||
+          !main_url ||
+          !short_url ||
+          !urlkey ||
+          !camp_id ||
+          !submit_via ||
+          !country_code
+        ) {
+          return res.status(400).json({
+            success: false,
+            message: "All fields are mandatory",
+          });
+        }
+    
+        const urlData = new urlShortLinkModel({
+          phone_number,
+          user_id,
+          sender,
+          main_url,
+          short_url,
+          urlkey,
+          created:  new Date(), 
+          ip: ip || null,
+          url_clickcount: url_clickcount || 0,
+          url_city: url_city || null,
+          url_device: url_device || null,
+          camp_id,
+          submit_via,
+          country_code,
+        });
+    
+        const savedData = await urlData.save();
+    
+        return res.status(200).json({
+          success: true,
+          message: "Data inserted successfully.",
+          data: savedData,
+        });
+    } else{
+        res.status(400).json({success:false,message:"invalid method"})
     }
-    const {
-      phone_number,
-      user_id,
-      sender,
-      main_url,
-      short_url,
-      urlkey,
-      created,
-      ip,
-      url_clickcount,
-      url_city,
-      url_device,
-      camp_id,
-      submit_via,
-      country_code,
-    } = resdata;
 
-    if (
-      !phone_number ||
-      !user_id ||
-      !sender ||
-      !main_url ||
-      !short_url ||
-      !urlkey ||
-      !camp_id ||
-      !submit_via ||
-      !country_code
-    ) {
-      return res.status(400).json({
-        success: false,
-        message: "All fields are mandatory",
-      });
-    }
-
-    const urlData = new urlShortLinkModel({
-      phone_number,
-      user_id,
-      sender,
-      main_url,
-      short_url,
-      urlkey,
-      created:  new Date(), 
-      ip: ip || null,
-      url_clickcount: url_clickcount || 0,
-      url_city: url_city || null,
-      url_device: url_device || null,
-      camp_id,
-      submit_via,
-      country_code,
-    });
-
-    const savedData = await urlData.save();
-
-    return res.status(200).json({
-      success: true,
-      message: "Data inserted successfully.",
-      data: savedData,
-    });
-  } catch (err) {
-    console.error("Error inserting data:", err);
-    return res.status(500).json({
-      success: false,
-      message: "An error occurred while inserting the data.",
-    });
-  }
 });
 
 module.exports = router;
