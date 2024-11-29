5606a6a25cbf3e5ae9bcfd0ab43ba5e8a6faaac6
kunal Lingwal <kunallingwal2@gmail.com>
2024-11-29 18:07:37 +0530

    Add files via upload

---
+++ Middleware/mongoAuth.js
@@ -0,0 +1,27 @@
+const db = require("../config/databaseconnection");
+const ErrorHandler = require("../Utils/ErrorHandler");
+
+const auth = async (req, res, next) => {
+  try {
+    const resdata = { ...req.query, ...req.body };
+
+    if (!resdata.user_id || !resdata.token) {
+      return next(new ErrorHandler("Missing User ID and Token", 400));
+    }
+
+    const query = "SELECT token FROM tbl_users WHERE id = ?";
+    const authData = await db(query, [resdata.user_id]);
+
+    if (!authData.length || authData[0].token !== resdata.token) {
+      return next(new ErrorHandler("Authentication Failed", 401));
+    }
+
+    req.user = authData[0];
+    next();
+  } catch (error) {
+    console.error("Error in Auth Middleware:", error);
+    return next(new ErrorHandler(error.message, 500));
+  }
+};
+
+module.exports = auth;
