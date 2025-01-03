e7eaa37c19e76d64df9cf72b6dc136dcef2ff90a
kunal Lingwal <kunallingwal2@gmail.com>
2025-01-03 15:36:24 +0530

    Update mongoconnection.js

--- config/mongoconnection.js
+++ config/mongoconnection.js
@@ -8,5 +8,8 @@ const mongodb_authkey_0kb=mongoose.createConnection(
 const mongodb_authkey_bulk = mongoose.createConnection(
   process.env.MONGODB_URI_AUTHKEY_BULK
 );
+const mongodb_authkey=mongoose.createConnection(
+  process.env.MONGODB_URI_AUTHKEY
+)
+module.exports = {  mongodb_authkey_bulk,mongodb_authkey_0kb,mongodb_authkey };
 
-module.exports = {  mongodb_authkey_bulk,mongodb_authkey_0kb };
